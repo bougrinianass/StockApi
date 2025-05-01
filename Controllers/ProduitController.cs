a1de317297914c6e85f6991bbe0364e8a53a4869
Anassbou92 <bougrinianasfb@gmail.com>
2025-05-01 16:04:28 +0200

    Premier commit - Création de l'API

---
+++ Controllers/ProduitController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+using ApiStock.Data;
+using ApiStock.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProduitController : ControllerBase
+    {
+        private readonly StockContext _context;
+
+        public ProduitController(StockContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produit>>> getProduits()
+        {
+            return await _context.Products.ToListAsync(); 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produit>> AddProduct(Produit produit)
+        {
+            _context.Products.Add(produit);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(getProduits), new { id = produit.Id },produit);
+        }
+    }
+}
---
+++ Data/StockContext.cs
@@ -0,0 +1,11 @@
+﻿using ApiStock.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStock.Data
+{
+    public class StockContext:DbContext
+    {
+        public StockContext(DbContextOptions<StockContext> options) : base(options) { }
+        public DbSet<Produit> Products { get; set; }
+    }
+}
